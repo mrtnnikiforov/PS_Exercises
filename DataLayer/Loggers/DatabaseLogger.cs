94c0f534a104b926122c44af057df25c37dd756c
mrtnnikiforov <mrtn.nkfrv@gmail.com>
2026-02-15 14:00:24 +0200

    Fourth Exercise changes

---
+++ DataLayer/Loggers/DatabaseLogger.cs
@@ -0,0 +1,37 @@
+﻿using DataLayer.Database;
+using DataLayer.Model;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DataLayer.Loggers
+{
+    public class DatabaseLogger : ILogger
+    {
+        
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+             return null; 
+        }
+
+       
+        public bool IsEnabled(LogLevel logLevel)
+        {
+             return true; 
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+          
+            using (var context = new DatabaseContext())
+            {
+                context.Logs.Add(new LogEntry
+                {
+                    Message = formatter(state, exception),
+                    Timestamp = DateTime.Now
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataLayer/Model/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLayer.Model
+{
+    public class LogEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
