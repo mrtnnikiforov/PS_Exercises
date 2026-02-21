13d954575c193f2c8aeac50e5c83ed4b87f4d052
mrtnnikiforov <mrtn.nkfrv@gmail.com>
2026-02-21 17:13:43 +0200

    Sixth Exercise before reorganisation of UI project

---
+++ WpfExample/NamesList.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WpfExample;
+
+public class NamesList : INotifyPropertyChanged
+{
+    string _firstName = "";
+    string _lastName = "";
+    string _selectedName;
+    public NamesList()
+    {
+        Names = new ObservableCollection<string>();
+    }
+    public string FirstName
+    {
+        get { return _firstName; }
+        set
+        {
+            if (_firstName != value)
+
+            {
+                _firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+    }
+    public string LastName
+    {
+        get { return _lastName; }
+        set
+        {
+            if (_lastName != value)
+            {
+                _lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+    }
+    public string SelectedName
+    {
+        get { return _selectedName; }
+        set
+        {
+            if (_selectedName != value)
+            {
+                _selectedName = value;
+                OnPropertyChanged("SelectedName");
+            }
+        }
+    }
+    public ObservableCollection<string> Names { get; private set; }
+    private void OnPropertyChanged(string property)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    AddCommand _addNameCommand = new AddCommand();
+    RemoveCommand _removeNameCommand = new RemoveCommand();
+
+    public RemoveCommand RemoveNameCommand
+    {
+        get { return _removeNameCommand; }
+    }
+    public AddCommand AddNameCommand
+    {
+        get { return _addNameCommand; }
+    }
+}
---
+++ WpfExample/RemoveCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfExample
+{
+    public class RemoveCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            var nameList = parameter as NamesList;
+            var oldName = nameList.SelectedName;
+            nameList.Names.Remove(oldName);
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
